e8a1d7667bf5953df75ab370c9cddc1c61b5c911
Gemma <gemmaehassan@gmail.com>
2022-05-13 21:47:39 +0100

    Initial Commit

---
+++ Shared/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  Shared
+//
+//  Created by Gemma Hassan on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
